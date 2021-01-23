0a578160107160c89f57e843f43f0a69c78c6467
Tharindu Madushan <77617483+tharindu6600@users.noreply.github.com>
2021-01-23 09:23:15 +0530

    Add files via upload

---
+++ Online exam 2/Version B/question2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <assert.h>
+
+float getDiscountPrice(int noOfGuests , float chargePerGuedt);
+float getAmount(int noOfGuests, float chargePerGuedt , float discount);
+
+int main()
+{
+	int noOfGuests;
+	float chargePerGuest;
+	
+	printf("Enter no of guests : ");
+	scanf("%d",&noOfGuests);
+	
+	printf("Enter charge per guest : ");
+	scanf("%f",&chargePerGuest);
+	
+	printf("Discount : %f\n",getDiscountPrice(noOfGuests , chargePerGuest));
+	printf("Amount to be paid : %f\n", getAmount(noOfGuests , chargePerGuest , getDiscountPrice(noOfGuests , chargePerGuest))); 
+		
+	return 0;
+}
+
+float getDiscountPrice(int noOfGuests , float chargePerGuest) // Calculate discount
+{
+	float hotelcharge = noOfGuests * chargePerGuest;
+	float discount;
+	
+	if(noOfGuests > 200)
+		discount = hotelcharge * 0.1;
+	else 
+		discount=0;
+	
+	return discount;		
+}
+
+float getAmount(int noOfGuests, float chargePerGuest , float discount) // Calculate TotalAmount to be paid
+{
+	float TotalAmount = (noOfGuests * chargePerGuest) - getDiscountPrice(noOfGuests,chargePerGuest);
+	
+	return TotalAmount;
+	
+}
