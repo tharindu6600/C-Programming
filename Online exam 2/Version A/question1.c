5fada9592e8452dd12c9d6ac8276a0e5df04a113
Tharindu Madushan <77617483+tharindu6600@users.noreply.github.com>
2021-01-23 09:07:53 +0530

    Add files via upload

---
+++ Online exam 2/Version A/question1.c
@@ -0,0 +1,21 @@
+/****************************************
+Online exam 2 - Version A - Question 1
+*****************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	
+	
+	for(int i=0;i<5;i++)
+	{	
+		for(int j=0;j<=i;j++)
+			printf("%c",97+i);
+		
+		
+		printf("\n");  
+		
+	}
+	return 0;
+}
