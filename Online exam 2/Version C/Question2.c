d03aab7893d9e8f00dfb62ffc78924b6570f7d78
Tharindu Madushan <77617483+tharindu6600@users.noreply.github.com>
2021-01-23 10:01:11 +0530

    Add files via upload

---
+++ Online exam 2/Version C/Question2.c
@@ -0,0 +1,49 @@
+/*****************************************
+Online exam 2 - Version C - Question 2
+******************************************/
+
+#include <stdio.h>
+
+float calculateWeeklySalary(int grade , float hrsWorked);
+void printDetails(int grade , float hrsWorked , float salary);
+
+int main()
+{
+	int empgrade;
+	float hoursworked , WeeklySalary;
+	
+	printf("Enter Employee Grade (1,2,3) : ");
+	scanf("%d",&empgrade);
+	printf("Enter no of hours worked : ");
+	scanf("%f",&hoursworked);
+	
+	WeeklySalary = calculateWeeklySalary(empgrade , hoursworked); 
+	printDetails(empgrade , hoursworked , WeeklySalary);
+
+	return 0;
+}
+
+float calculateWeeklySalary(int grade , float hrsWorked)  // Calculate weekly salary
+{
+	int hourlyrate;
+	float WeeklySalary;
+	
+	if(grade == 1)
+		hourlyrate = 100;
+	
+	else if(grade == 2)
+		hourlyrate = 200;
+	
+	else if(grade == 3)
+		hourlyrate = 300;
+	
+	WeeklySalary = hrsWorked * hourlyrate;
+	
+	return WeeklySalary;
+	
+}
+
+void printDetails(int grade , float hrsWorked , float salary) 
+{
+	printf("Grade : %d \nHours Worked : %.2f \nWeekly Salary : %.2f ", grade , hrsWorked , salary );
+}
