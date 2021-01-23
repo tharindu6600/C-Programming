72f5f23c3ec9376313b7e2263deeac7e50216e9b
Tharindu Madushan <77617483+tharindu6600@users.noreply.github.com>
2021-01-23 09:43:11 +0530

    Add files via upload

---
+++ Online exam 2/Version C/Question1.c
@@ -0,0 +1,21 @@
+/****************************************
+Online exam 2 - Version 2 - Question 1
+*****************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	int i,j,k, value=2;
+	
+	for( i=1; i <= value; i++) {
+		for( j=1; j <= value; j++){
+			for( k=1; k <= value; k++)
+				printf("[ %d %d %d ] \n", i, j, k);
+		
+	
+	
+		}
+	}
+	return 0;
+}
